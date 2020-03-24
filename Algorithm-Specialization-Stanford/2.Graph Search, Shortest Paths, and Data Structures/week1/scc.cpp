6d1f5717984983fd1f5c0efdd899bbc42c9ff11a
usernamegenerator <bigship1990@gmail.com>
2020-03-23 22:27:20 -0600

    adding test cases, there are some wired error need debugger

--- Algorithm-Specialization-Stanford/2.Graph Search, Shortest Paths, and Data Structures/week1/scc.cpp
+++ Algorithm-Specialization-Stanford/2.Graph Search, Shortest Paths, and Data Structures/week1/scc.cpp
@@ -2,6 +2,9 @@
 // https://www.cs.princeton.edu/courses/archive/fall12/cos226/lectures/42DirectedGraphs.pdf
 // https://www.coursera.org/lecture/algorithms-part2/strong-components-fC5Yw
 
+// v and w are strongly connected if there is both a directed path from v to w
+// and a directed path from w to v
+
 #include <iostream>
 #include <list>
 #include <vector>
@@ -43,10 +46,10 @@ void printList(list<int> l)
     1. compute G_reverse
     2. Compute topological order (reverse postorder) in G_reverse.
         DFS in G_reverse, return a list
-            the list has the the first done node at the end
-                create a stack, run DFS, if a node is done, push to stack
-                pop the stack and push_back to the list
-                the list is the topology order of G_reverse
+        the list has the first done node at the end
+            create a stack, run DFS, if a node is done, push to stack
+            pop the stack and push_back to the list
+            the list is the topology order of G_reverse
     3. Run DFS in G, visiting unmarked vertices in reverse postorder(topological order) of G_reverse
 */
 class KosarajuSCC
@@ -70,53 +73,73 @@ KosarajuSCC::KosarajuSCC(string filename)
 {
     this->graph = buildGraphFromtxt(filename);
     this->graphSize = this->graph.size();
+
+    printGraph(graph);
+
     // 1. compute G_reverse
     graph_reverse = reverseGraph(graph);
 
-    //printGraph(graph);
+    
     //printGraph(graph_reverse);
-
-
-
 
     // 2. topology order of the G_reverse
     list<int> topologyorderList = TopologyOrder(graph_reverse);
 
-
     // 3.
     bool isMarked[graphSize] = {false};
     int numOfscc = 0;
+    // init to graphSize in case worst case that every node is scc by itself
+    // scc[u]'s value is which scc this u belongs to
     int scc[graphSize] = {-1};
     for (list<int>::iterator it = topologyorderList.begin(); it != topologyorderList.end(); it++)
     {
         if (!isMarked[*it])
         {
             sccDFS(graph, *it, isMarked, scc, numOfscc);
+            // every time the above scc returns and run to this line.
+            // it means start with this node *it, the DFS has exhausted
+            // so it means this scc ends. need to increase the numOfscc to record next
             numOfscc++;
         }
     }
+    /*
     for (int i = 0; i < graphSize; i++)
     {
         cout << scc[i] << " ";
     }
     cout << endl;
+    */
 
     // 4. for this assignment only
     // count the size of each scc
     int sizeOfscc[numOfscc] = {0};
     for (int i = 0; i < graphSize; i++)
     {
+        // scc[i] means i belongs to which scc
         sizeOfscc[scc[i]]++;
     }
     sort(sizeOfscc, sizeOfscc + numOfscc);
-    for (int i = 0; i < 5; i++)
+    if (numOfscc >= 5)
     {
-        cout << sizeOfscc[numOfscc - 1 - i] << " ";
+        for (int i = 0; i < 5; i++)
+        {
+            cout << sizeOfscc[numOfscc - 1 - i] << " ";
+        }
     }
+    else
+    {
+        int i = 0;
+        for (; i < numOfscc; i++)
+        {
+            cout << sizeOfscc[numOfscc - 1 - i] << " ";
+        }
+        for(;i<5;i++)
+        {
+            cout << "0" << " ";
+        }
+    }
+    
     cout << endl;
-    
-    
-    
 }
 
 list<int> KosarajuSCC::TopologyOrder(vector<list<int>> g)
@@ -137,7 +160,7 @@ list<int> KosarajuSCC::TopologyOrder(vector<list<int>> g)
         stk.pop();
     }
 
-    printList(topologyorderList);
+    // printList(topologyorderList);
 
     return topologyorderList;
 }
@@ -200,12 +223,20 @@ vector<list<int>> KosarajuSCC::buildGraphFromtxt(string filename)
     while (getline(fin, line))
     {
         stringstream ss(line);
+        cout << "line" << line << endl;
         ss >> u_str;
+        cout << u_str << endl;
         ss >> v_str;
+        cout << v_str << endl;
         u = stoi(u_str) - 1;
+        cout << u << endl;
         v = stoi(v_str) - 1;
+        cout << v << endl;
+        cout << u << "->" << v << " ";
         graph[u].push_back(v);
+        cout << u << "->" << v << endl;
     }
+    printGraph(graph);
     return graph;
 }
 
@@ -229,6 +260,13 @@ vector<list<int>> KosarajuSCC::reverseGraph(vector<list<int>> graph)
 
 int main()
 {
-    KosarajuSCC k("smallSCC.txt");
+    //KosarajuSCC k("smallSCC54211.txt");
+    //KosarajuSCC k("33300.txt");
+    //KosarajuSCC k("33200.txt");
+    //KosarajuSCC k("63210.txt");
+    
+
+    KosarajuSCC k("33110.txt");
+    //KosarajuSCC k("71000.txt");
     //KosarajuSCC k("SCC.txt");
 }
